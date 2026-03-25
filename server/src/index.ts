0b66a75d9730a1bf6ee2078cb8982afb446c49ad
Gnarkill33 <gnarkill33@gmail.com>
2026-03-25 18:32:31 +0300

    feat: add websocket events and incoming message handler

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,48 @@
-import { WebSocketServer } from 'ws';
-
+import { WebSocketServer } from "ws";
+import { playerStore } from "./store";
+import { WSMessage } from "./types";
+import { createRegResponse } from "./handlers";
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 // WebSocket server
 const wss = new WebSocketServer({ port: PORT });
+
+wss.on("listening", () => {
+ console.log(`Websocket server started on port ${PORT}`);
+});
+
+wss.on("connection", (ws) => {
+ console.log("New client connected");
+
+ ws.on("error", console.error);
+
+ ws.on("message", async (message) => {
+  try {
+   const parsedMessage = JSON.parse(message.toString());
+   const response = await handleIncomingMessage(parsedMessage);
+
+   if (response) {
+    ws.send(JSON.stringify(response));
+   }
+  } catch (error) {
+   console.error(error);
+  }
+ });
+
+ ws.on("close", () => {
+  console.log("Client disconnected");
+ });
+});
+
+export const handleIncomingMessage = async (message: WSMessage) => {
+ const { type, data } = message;
+
+ const parsedData = typeof data === "string" ? JSON.parse(data) : data;
+
+ switch (type) {
+  case "reg":
+   const registeredPlayer = playerStore.addPlayer(parsedData);
+   return createRegResponse(registeredPlayer);
+ }
+};
