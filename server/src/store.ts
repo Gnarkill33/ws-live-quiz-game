07b87c3dafbffda4f2b5c51983e9861a92a8b045
Gnarkill33 <gnarkill33@gmail.com>
2026-03-25 20:32:32 +0300

    feat: add games store

--- server/src/store.ts
+++ server/src/store.ts
@@ -1,4 +1,6 @@
-import { RegData } from "./types";
+import { randomUUID } from "crypto";
+import { Game, Question, RegData, WebSocketWithId } from "./types";
+import { generateGameCode } from "./utils";
 
 class Players {
  private players = new Map();
@@ -13,3 +15,31 @@ class Players {
 }
 
 export const playerStore = new Players();
+
+class Games {
+ private games = new Map();
+
+ createGame(questions: Question[], ws: WebSocketWithId) {
+  const gameId = randomUUID();
+  const code = generateGameCode();
+
+  const newGame: Game = {
+   id: gameId,
+   code: code,
+   hostId: ws.id,
+   questions: questions,
+   players: [],
+   currentQuestion: 0,
+   status: "waiting",
+   questionStartTime: undefined,
+   questionTimer: undefined,
+   playerAnswers: new Map(),
+  };
+
+  this.games.set(gameId, newGame);
+
+  return newGame;
+ }
+}
+
+export const gameStore = new Games();
