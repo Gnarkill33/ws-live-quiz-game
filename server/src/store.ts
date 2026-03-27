537f7c22379bebbaffa110b1924594060b71f102
Gnarkill33 <gnarkill33@gmail.com>
2026-03-28 00:00:08 +0300

    feat: add methods and player and game stores

--- server/src/store.ts
+++ server/src/store.ts
@@ -1,16 +1,33 @@
 import { randomUUID } from "crypto";
-import { Game, Question, RegData, WebSocketWithId } from "./types";
+import {
+ CreateGameData,
+ Game,
+ JoinGameData,
+ RegData,
+ StartGameData,
+ User,
+ WebSocketWithId,
+ WSMessage,
+} from "./types";
 import { generateGameCode } from "./utils";
 
 class Players {
  private players = new Map();
  private playerIndex = 0;
 
- addPlayer(player: RegData) {
-  const newPlayer = { ...player, index: String(this.playerIndex++) };
+ addPlayer(player: RegData, ws: WebSocketWithId): User {
+  const newPlayer = { ...player, ws: ws, index: String(this.playerIndex++) };
   this.players.set(player.name, newPlayer);
 
   return newPlayer;
+ }
+
+ getPlayerByWsId(ws: WebSocketWithId) {
+  for (const player of this.players.values()) {
+   if (player.ws.id === ws.id) {
+    return player;
+   }
+  }
  }
 }
 
@@ -19,7 +36,7 @@ export const playerStore = new Players();
 class Games {
  private games = new Map();
 
- createGame(questions: Question[], ws: WebSocketWithId) {
+ createGame(data: CreateGameData, ws: WebSocketWithId) {
   const gameId = randomUUID();
   const code = generateGameCode();
 
@@ -27,7 +44,8 @@ class Games {
    id: gameId,
    code: code,
    hostId: ws.id,
-   questions: questions,
+   hostWs: ws,
+   questions: data.questions,
    players: [],
    currentQuestion: 0,
    status: "waiting",
@@ -39,6 +57,53 @@ class Games {
   this.games.set(gameId, newGame);
 
   return newGame;
+ }
+
+ findGameByCode(gameCode: string): Game | null {
+  for (const game of this.games.values()) {
+   if (game.code === gameCode) {
+    return game;
+   }
+  }
+  return null;
+ }
+
+ findGameById(gameId: string): Game | null {
+  for (const game of this.games.values()) {
+   if (game.id === gameId) {
+    return game;
+   }
+  }
+  return null;
+ }
+
+ findGameHost(gameId: string) {
+  const game = this.findGameById(gameId);
+  return game?.hostWs;
+ }
+
+ joinGame(data: JoinGameData, ws: WebSocketWithId) {
+  const gameToJoin = this.findGameByCode(data.code);
+  const existingPlayer = playerStore.getPlayerByWsId(ws);
+
+  if (!gameToJoin) return null;
+
+  gameToJoin.players.push(existingPlayer);
+
+  existingPlayer.score = 0;
+
+  gameToJoin.playerAnswers.set(existingPlayer.name, {
+   answerIndex: 0,
+   timestamp: 0,
+  });
+
+  return gameToJoin;
+ }
+
+ startGame(data: StartGameData, ws: WebSocketWithId) {
+  const gameStarted = this.findGameById(data.gameId);
+  if (gameStarted) gameStarted.status = "in_progress";
+  return gameStarted;
  }
 }
 
